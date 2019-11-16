c4ad153712cbd992aa4b5419e52414d42ad72cca
RauhoferE <104609@fhwn.ac.at>
2019-11-16 19:02:39 +0100

    updated hub

--- SnakeServer/Hubs/SnakeHub.cs
+++ SnakeServer/Hubs/SnakeHub.cs
@@ -6,9 +6,33 @@ using System.Threading.Tasks;
 
 namespace SnakeServer
 {
+    using Microsoft.Extensions.Logging;
+    using Snake_V_0_3;
+
     public class SnakeHub : Hub
     {
-        public SnakeHub()
+        private Application game;
+
+        private ILogger<SnakeHub> logger;
+
+        public SnakeHub(ILogger<SnakeHub> ilogger)
+        {
+            this.game = new Application();
+            this.logger = ilogger;
+            //Events
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task OnInput()
         {
 
         }
